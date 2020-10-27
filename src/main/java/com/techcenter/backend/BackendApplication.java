6127f6df6e95ea900073bb0951098deb14ed480b
Kamel <Kamel.mahjoub@outlook.com>
2020-10-27 17:12:26 +0100

    Added cors support

--- src/main/java/com/techcenter/backend/BackendApplication.java
+++ src/main/java/com/techcenter/backend/BackendApplication.java
@@ -2,12 +2,18 @@ package com.techcenter.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@Configuration
 public class BackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
+
+
 
 }
